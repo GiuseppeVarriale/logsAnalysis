ef0fb7c289c122c043b6fc1d5595f030d8f6c3f0
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-02-05 22:47:02 +0000

    Feat: ADD Return most popular authors return function

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -21,6 +21,22 @@ def get_popular_articles():
     return popularArticles
 
 
+def get_popular_authors():
+    """Return the most popular article authors of all time, the most popular
+     author at the top."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select name, count(b.status) as views from articles as a, log as b, \
+        authors as c where b.path like '/article/' || a.slug and \
+        b.status = '200 OK' and c.id = a.author group by name \
+        order by views desc"
+    )
+    popularAuthors = c.fetchall()
+    db.close()
+    return popularAuthors
+
+
 if __name__ == "__main__":
     print(get_popular_articles())
     print(get_popular_authors())
